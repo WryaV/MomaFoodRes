e3805231f76eea1d59f6ae6f1545dbf8eba41e71
WryaV <48864373+WryaV@users.noreply.github.com>
2025-01-30 20:23:33 +0100

    initialization

---
+++ MomaFoodRes/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MomaFoodRes.Models
+{
+    public class AppUser: IdentityUser
+    {
+        public ICollection<Order> Orders { get; set; }  
+    }
+}
